f8d0257eff35f66ca669450539e09c5c60e14c5e
Nathan Chan <123600671+nathanc726@users.noreply.github.com>
2023-04-05 14:18:48 -0600

    made some change

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Nathan Chan
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,8 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
-
+let modelName = "XYZ";
+let duration = 0;
 
 
 /****************** helper function ******************/
@@ -27,9 +27,20 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+function recalculate() {
+    let costLabel = document.getElementByld("calculated-cost");
+    let totalCost = 0;
 
+    if (modelName === "XYZ") {
+        totalCost = duration * 100;
+    }
 
+    else if (modelName === "CPRG") {
+        totalCost = duration * 213;
+    }
 
+    costLabel.innerHTML = totalCost;
+}
 
 
 /****************** model button logic ******************/
@@ -46,10 +57,21 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let modelButton = document.getElementById("model-button");
 
+function changeModel() {
+    let modelText = document.getElementById("model-text");
+    if (modelName === "XYZ") {
+        modelName = "CPRG";
+        modelText.innerHTML = "Model CPRG";
+    }
+    else if (modelName === "CPRG") {
+        modelName = "XYZ";
+        modelText.innerHTML = "Model XYZ";
+    }
 
-
-
+recalculate();
+}
 
 
 /****************** duration button logic ******************/
